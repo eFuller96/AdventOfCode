771aa88344441245113509a864461c1c5cfe84d3
Ellie Fuller <ellie.fuller@thetrainline.com>
2022-12-01 14:26:51 +0000

    Day 1 solution

---
+++ Day1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+var listOfCalories = new List<int>();
+var calorieCounter = 0;
+
+using (var streamReader = File.OpenText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "input.txt")))
+{
+    while (!streamReader.EndOfStream)
+    {
+        var line = streamReader.ReadLine();
+        if (line == string.Empty)
+        {
+            listOfCalories.Add(calorieCounter);
+            calorieCounter = 0;
+            continue;
+        }
+
+        calorieCounter += int.Parse(line);
+    }
+}
+
+var answer = listOfCalories.Max();
+
+Console.WriteLine($"answer:{answer}");
+
+var answerTwo = listOfCalories.OrderByDescending(x => x).Take(3).Sum();
+
+Console.WriteLine($"answer 2: {answerTwo}");
