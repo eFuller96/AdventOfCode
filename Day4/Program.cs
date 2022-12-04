49487b19ad740ec61a3cb3bf206d8946184d1475
Ellie Fuller <ellie.fuller@thetrainline.com>
2022-12-04 11:57:32 +0000

    Day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+using var streamReader =
+    File.OpenText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "input.txt"));
+
+var count = 0;
+
+while (!streamReader.EndOfStream)
+{
+    var line = streamReader.ReadLine();
+    var pairs = line!.Split(',')
+        .Select(x => new Tuple<int, int>(int.Parse(x.Split('-').First()), int.Parse(x.Split('-').Last()))).ToArray();
+
+    var firstRange = pairs.First();
+    var secondRange = pairs.Last();
+
+    // Star 1
+    // if ((firstRange.Item1 - secondRange.Item1 > 0 && firstRange.Item2 - secondRange.Item2 > 0) ||
+    //     firstRange.Item1 - secondRange.Item1 < 0 && firstRange.Item2 - secondRange.Item2 < 0)
+    // {
+    //     continue;
+    // }
+
+    // Star 2
+    if ((secondRange.Item1 < firstRange.Item1 && secondRange.Item1 < firstRange.Item2 &&
+         secondRange.Item2 < firstRange.Item1 && secondRange.Item2 < firstRange.Item2) ||
+        (secondRange.Item2 > firstRange.Item1 && secondRange.Item2 > firstRange.Item2 &&
+         secondRange.Item1 > firstRange.Item1 && secondRange.Item1 > firstRange.Item2))
+    {
+        continue;
+    }
+
+    count++;
+}
+
+Console.WriteLine($"answer: {count}");
