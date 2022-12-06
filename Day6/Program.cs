6243fa46e230f61863e240aa83bd6bddf38ceebf
Ellie Fuller <ellie.fuller@thetrainline.com>
2022-12-06 14:15:51 +0000

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+
+char answer;
+
+const int firstStar = 4;
+const int secondStar = 14;
+
+Get(firstStar);
+Get(secondStar);
+
+void Get(int length)
+{
+    using var streamReader =
+        File.OpenText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "input.txt"));
+
+    while (!streamReader.EndOfStream)
+    {
+        var line = streamReader.ReadLine();
+
+        for (var i = 0; i < line!.Length; i++)
+        {
+            var windowToCheck = line.Substring(i, length);
+            if (windowToCheck.Distinct().Count() == length)
+            {
+                Console.WriteLine($"answer: {i+length}");
+                break;
+            }
+        }
+    }
+}
